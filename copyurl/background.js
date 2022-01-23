54d9ff1cca67a5e5ff0e9e78193c45eac27457b2
Kotaro Ichihara <taro3.ichi@gmail.com>
2022-01-23 11:08:37 +0900

    copyurl v0.2.0 (#1)
    * add feature: to deactivate menu
    
    * add updatemenu functions
    
    * create option page
    
    * implement options

--- copyurl/background.js
+++ copyurl/background.js
@@ -1,26 +1,66 @@
 "use strict";
 
-import { menus } from "./modules/background/menus.js"
+import { menus as defaultMenus } from "./modules/background/menus.js"
 
-chrome.runtime.onInstalled.addListener(createContextMenus);
+chrome.runtime.onInstalled.addListener(initializeMenus);
+chrome.runtime.onMessage.addListener(refreshMenus);
 
-function createContextMenus() {
-  for (const key in menus) {
-    chrome.contextMenus.create(
-      {
-        id: key,
-        title: menus[key]["title"],
+chrome.contextMenus.onClicked.addListener(runTaskOfClickedMenu);
+
+function initializeMenus(details) {
+  chrome.storage.sync.get("menus")
+    .then((items) => {
+      let menus;
+      if (items.menus) {
+        menus = items.menus;
+      } else {
+        menus = defaultMenus;
       }
-    );
+      chrome.storage.sync.set({ menus })
+        .then(createContextMenus(menus));
+    });
+}
+
+function refreshMenus(message, sender, sendResponse) {
+  if (!message.refresh) {
+    sendResponse({ result: 'refreshMenus did nothing' });
+    return
   }
-  chrome.contextMenus.onClicked.addListener(runTaskOfClickedMenu);
+  updateContextMenus();
+  sendResponse({ result: 'finshed' });
+}
+
+function createContextMenus(menus) {
+  for (const key in menus) {
+    if (menus[key]["active"]) {
+      chrome.contextMenus.create(
+        {
+          id: key,
+          title: menus[key]["title"],
+        }
+      );
+    }
+  }
+}
+
+function updateContextMenus() {
+  chrome.contextMenus.removeAll();
+  chrome.storage.sync.get("menus")
+    .then((items) => {
+      if (items.menus) {
+        createContextMenus(items.menus);
+      } else {
+        createContextMenus(defaultMenus);
+        console.log("update failed, fallback to default menu")
+      }
+    });
 }
 
 function runTaskOfClickedMenu(info, tab) {
   const id = info.menuItemId;
-  if (!(id in menus)) {
+  if (!(id in defaultMenus)) {
     throw ("an undefined item of menus.")
   }
-  const task = menus[id]["task"];
+  const task = defaultMenus[id]["task"];
   task();
 }
---
+++ copyurl/options/options.js
@@ -0,0 +1,42 @@
+"use strict";
+
+chrome.storage.sync.get("menus")
+  .then((items) => {
+    const menus = items.menus;
+    const options = document.getElementById("options");
+    for (const key in menus) {
+      const menuDiv = document.createElement("div");
+      options.appendChild(menuDiv);
+
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.id = key;
+      checkbox.checked = menus[key]["active"];
+
+      const label = document.createElement("label");
+      label.for = key;
+      label.innerText = menus[key]["title"];
+
+      menuDiv.appendChild(checkbox);
+      menuDiv.appendChild(label);
+
+      checkbox.addEventListener("change", (event) => {
+        updateMenus();
+      })
+    }
+  });
+
+function updateMenus() {
+  chrome.storage.sync.get("menus")
+    .then((items) => {
+      const menus = items.menus;
+      for (const key in menus) {
+        const checkbox = document.getElementById(key);
+        menus[key]["active"] = checkbox.checked;
+      }
+      chrome.storage.sync.set({ menus }).then(() => {
+        chrome.runtime.sendMessage({ refresh: true })
+      });
+    });
+}
+
