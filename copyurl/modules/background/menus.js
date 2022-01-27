bf43b918d8344cd29a255f8bd58a94a9112e4c76
ichi_taro3 <taro3.ichi@gmail.com>
2022-01-27 23:04:40 +0900

    modify menu

--- copyurl/modules/background/menus.js
+++ copyurl/modules/background/menus.js
@@ -7,53 +7,53 @@ import {
 
 const menus = [
   {
+    id: "copyRichLink",
+    title: "rich text link",
+    task: copyRichLink,
+    active: true,
+    description: "Copies the URL as a rich text link, so that you can paste the link with the title to your document(e.g. Google docs...) WITHOUT any editing.",
+  },
+  {
     id: "copyurl",
-    title: "copy only url",
+    title: "copy url",
     task: copyUrl,
     active: true,
     description: "Copies the URL of the page to the clipboard.",
   },
   {
     id: "copyurl_titletext",
-    title: "copy url with the title",
+    title: "url with title",
     task: copyUrlWithTitleAsText,
     active: true,
     description: "Copies the URL and TITLE of the page, they are separated with `|`.",
   }, {
     id: "copyurl_markdown",
-    title: "copy url and title as markdown",
+    title: "url and title as markdown",
     task: copyUrlWithTitleAsMarkdown,
     active: true,
     description: "Copies the URL and TITLE as a markdown-style link.",
   },
   {
     id: "copyurl_html",
-    title: "copy url as html link",
+    title: "html link",
     task: copyUrlAsHtml,
     active: false,
     description: "Copies the URL as HTML-style anchor link(a tag).",
   },
   {
     id: "copyurl_html_title",
-    title: "copy url as html link with title",
+    title: "html link with title",
     task: copyUrlWithTitleAsHtml,
     active: false,
     description: "Copies the URL and TITLE as an HTML-style anchor link (a tag).",
   },
   {
     id: "copytitle",
-    title: "copy title only",
+    title: "title only",
     task: copyTitle,
     active: true,
     description: "Copies the TITLE of the page.",
   },
-  {
-    id: "copyRichLink",
-    title: "copy url as rich text link",
-    task: copyRichLink,
-    active: true,
-    description: "Copies the URL as a rich text link, so that you can paste the link with the title to your document(e.g. Google docs...) WITHOUT any editing.",
-  }
 ];
 
 export { menus };
