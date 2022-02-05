5f26008b73565c9004110ba79a264fab1c3b4b51
Kotaro Ichihara <taro3.ichi@gmail.com>
2022-02-05 14:06:18 +0900

    v0.4.0 (#6)
    * add feature: to deactivate menu
    
    * add updatemenu functions
    
    * create option page
    
    * implement options
    
    * copyurl with rich text
    
    * also changed the menus' titles
    
    * added description of the menusd
    
    * v0.3.0
    
    * multi mime type copying
    
    * changed menu data structure to maintain sort
    
    * refactor menus
    
    * increment patchversion
    
    * bug fix: expand title in markdown style link
    
    * run copy func under the activeTab permission
    
    instead of the content-script

---
+++ copyurl/modules/background/Copy.js
@@ -0,0 +1,115 @@
+"use strict";
+// This script tend to run in the context of the user's tab
+// by chrome.scripting.executeScript().
+
+async function Copy(task) {
+  async function copyUrl() {
+    const content = location.href;
+    await writeToClipboard(content);
+  }
+
+  async function copyUrlWithTitleAsText() {
+    const content = `${document.title} | ${location.href}`;
+    await writeToClipboard(content);
+  }
+
+  async function copyUrlWithTitleAsMarkdown() {
+    const content = `[${document.title}](${location.href})`;
+    await writeToClipboard(content);
+  }
+
+  async function copyUrlAsHtml() {
+    const a = document.createElement("a");
+    a.href = location.href;
+    const content = a.outerHTML;
+    await writeToClipboard(content);
+  }
+
+  async function copyUrlWithTitleAsHtml() {
+    const a = document.createElement("a");
+    a.href = location.href;
+    a.innerText = document.title;
+    const content = a.outerHTML;
+    await writeToClipboard(content);
+  }
+
+  async function copyTitle() {
+    const content = document.title;
+    await writeToClipboard(content);
+  }
+
+  async function copyRichLink() {
+    const a = document.createElement("a");
+    a.href = location.href;
+    a.innerText = document.title;
+    await writeRichTextToClipboard(a);
+  }
+
+  async function writeToClipboard(content) {
+    try {
+      await navigator.clipboard.writeText(content);
+    } catch (e) {
+      console.debug("copying failed. Trying to fall back to execCommand('copy')");
+      const textArea = document.createElement("textArea");
+      textArea.textContent = content;
+      document.body.append(textArea);
+      textArea.select();
+      document.execCommand('copy');
+      textArea.remove();
+    }
+  }
+
+  async function writeRichTextToClipboard(content) {
+    const htmlblob = new Blob([content.outerHTML], { type: "text/html" });
+    const textblob = new Blob([content.href], { type: "text/plain" });
+    try {
+      await navigator.clipboard.write([
+        new ClipboardItem({
+          [htmlblob.type]: htmlblob,
+          [textblob.type]: textblob,
+        })
+      ]);
+    } catch (e) {
+      console.debug(e);
+      console.debug("copying failed. Trying to fall back to execCommand('copy')");
+
+      function listener(event) {
+        event.preventDefault();
+        event.clipboardData.setData("text/html", content.outerHTML);
+        event.clipboardData.setData("text/plain", content.href);
+      }
+      document.addEventListener("copy", listener, { passive: false });
+      document.execCommand('copy');
+      document.removeEventListener("copy", listener);
+    }
+  }
+
+
+  switch (task) {
+    case "copyUrl":
+      await copyUrl();
+      break;
+    case "copyUrlWithTitleAsText":
+      await copyUrlWithTitleAsText();
+      break;
+    case "copyUrlWithTitleAsMarkdown":
+      await copyUrlWithTitleAsMarkdown();
+      break;
+    case "copyUrlAsHtml":
+      await copyUrlAsHtml();
+      break;
+    case "copyUrlWithTitleAsHtml":
+      await copyUrlWithTitleAsHtml();
+      break;
+    case "copyTitle":
+      await copyTitle();
+      break;
+    case "copyRichLink":
+      await copyRichLink();
+      break;
+    default:
+      console.debug("not implemented");
+  }
+}
+
+export { Copy }
