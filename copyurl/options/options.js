ca50f7db45db991417e7b22d49c5b6d2421643d7
ichi_taro3 <taro3.ichi@gmail.com>
2022-02-06 21:55:10 +0900

    supporting japanese

--- copyurl/options/options.js
+++ copyurl/options/options.js
@@ -15,11 +15,18 @@ chrome.storage.sync.get("contextMenus")
 
       const label = document.createElement("label");
       label.for = menu["id"];
-      label.innerText = menu["title"];
+      // Until the chrome bug is fixed, you cannot i18n-ized context menu.
+      // c.f. https://bugs.chromium.org/p/chromium/issues/detail?id=1268098
+      // So adding some translations beside to English title
+      if (chrome.i18n.getUILanguage().slice(0, 2) === "en") {
+        label.innerText = menu["title"];
+      } else {
+        label.innerText = menu["title"] + ` (${chrome.i18n.getMessage(menu.id)})`;
+      }
 
       const p = document.createElement("p");
-      p.className = "description"
-      p.innerText = menu["description"];
+      p.className = "description";
+      p.innerText = chrome.i18n.getMessage(`${menu.id}_description`);
 
       menuDiv.appendChild(checkbox);
       menuDiv.appendChild(label);
